3d543dbf83e85eeda3ffc819195b6a318f16f792
Magnus Bågenholm <magnus@wynnit.se>
2021-02-26 09:02:07 +0100

    Added build

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const userRoutes = require("./src/models/users/UserRoute");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 // Cors
 
 const corsOptions = {
